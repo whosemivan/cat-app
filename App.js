891a64a3d6bb4168af4ed7bdd8eb5c89ac97543b
Ivan Semyonenko <alienba6y@Ivalienba6y.local>
2022-06-08 18:02:34 +0300

    adding axios and do one example query

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 import Auth from './components/Auth/auth';
 import Main from './components/Main/main';
 
 
 export default function App() {
+  const [login, setLogin] = useState('alienba6y');
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getCats = () => {
+        axios
+            .get(`http://sb-cats.herokuapp.com/api/2/${login}/show`)
+            .then((response) => {
+                console.log(response.status);
+                console.log(response.data);
+                setData(data);
+            })
+            .catch((e) => console.log('something went wrong :(', e));
+    };
+    getCats();
+}, []);
+
+
   return (
     <View style={styles.container}>
       <Main/>
+      <Text>
+      </Text>
     </View>
   );
 }
